5206268f869eb1d27686d047bb257510d11868ef
Nagarjun <deepaknagarjun@gmail.com>
2019-09-20 18:28:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,6 @@ generateBtn.addEventListener('click', function () {
     a.appendChild(linkText);
     a.title = "xAPI Bookmark";
     a.classList.add('mt-3');
-    a.href = "javascript: function rbm(){window.rbmAuth='Basic ZmU3YTdiYTAyZDU4NmM6Yjc5ZGVmMmZkMWY5ZWQ = ',window.rbmEndpoint='https://watershedlrs.com/api/organizations/8501/lrs/',window.rbmEmail='deepaknagarjun@gmail.com',window.rbmName='Nagarjun';var a=document,b=a.createElement('sc'+'ript'),c=a.body,d=a.location;b.setAttribute('src','https://cdn.jsdelivr.net/gh/nagarjundeepak/xAPI/bookmark.js');c.appendChild(b)}rbm();void 0";
+    a.href = `javascript: function rbm(){window.rbmAuth="Basic ZmU3YTdiYTAyZDU4NmM6Yjc5ZGVmMmZkMWY5ZWQ=",window.rbmEndpoint="https://watershedlrs.com/api/organizations/8501/lrs/",window.rbmEmail="deepaknagarjun@gmail.com",window.rbmName="Nagarjun";var a=document,b=a.createElement("sc"+"ript"),c=a.body,d=a.location;b.setAttribute("src","https://cdn.jsdelivr.net/gh/nagarjundeepak/xAPI/test.js");c.appendChild(b)}rbm();void 0`;
     document.body.append(a);
 });
